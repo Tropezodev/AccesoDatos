211897e9dd93be38ba3c3226fc8f1101fd18a97c
Cristian <cristian.perez.mail@gmail.com>
2023-10-10 09:02:37 +0200

    Fichajes

--- Fichaje/src/main/java/com/example/fichaje/Trabajador.java
+++ Fichaje/src/main/java/com/example/fichaje/Trabajador.java
@@ -1,10 +1,50 @@
 package com.example.fichaje;
 
-import lombok.Data;
 
-@Data
+
 public class Trabajador {
     private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
     private String nombre;
     private String apellidos;
     private String dni;
