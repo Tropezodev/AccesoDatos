00010b120726ca9c947f38c1dcae34ee17b74400
Cristian <cristian.perez.mail@gmail.com>
2023-12-04 13:15:29 +0100

    Mucho

--- Hibernate Maven 01/src/main/java/Libro.java
+++ Hibernate Maven 01/src/main/java/Libro.java
@@ -9,9 +9,12 @@ public class Libro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    String
-
-
+    // El @Column es para añadirle parámetros a la columna, ya las crea con el nombre del String automáticamente.
+    @Column(nullable = false) // nullable=false es para que se ponga el título a la fuerza
+    private String titulo;
+    private String autor;
+    @ManyToOne // Se establece la relación entre las clases Libro y Editorial. Se devuelve la Editorial de cada libro
+    private Editorial editorial;
 
     public Long getId() {
         return id;
@@ -19,6 +22,33 @@ public class Libro {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Editorial getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(Editorial editorial) {
+        this.editorial = editorial;
+    }
+
+    public Libro() {
     }
 }
 
